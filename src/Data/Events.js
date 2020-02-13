39964f72f51426b9042ad265fc00c6a1b9c85f92
Sean Ross <sean.ross.985@gmail.com>
2020-02-13 12:43:26 -0800

    Added event data

---
+++ src/Data/Events.js
@@ -0,0 +1,56 @@
+const event_data = {
+    "data": [
+        {
+            "title": "Sarah's D&D Group",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Sarah's D&D Group",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Sarah's D&D Group",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Sarah's D&D Group",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Sarah's D&D Group",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        }
+        
+            ]
+}
+export default event_data;
