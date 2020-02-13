0aeee63516cea1cc5d3d50c6e862623a67acdda6
kennansalisbury <kennansalisbury@gmail.com>
2020-02-13 11:55:54 -0800

    removed react example code and installed css frameworks and react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Container} from 'reactstrap'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container>
+      <header>
+        STUB NAV
       </header>
-    </div>
+      <main>
+        STUB MAIN PAGE CONTENT
+      </main>
+      <footer>
+        STUB FOOTER
+      </footer>
+    </Container>
   );
 }
 
