6a90f2bf1568bfb4b39f6a8b2df64afbc1965a10
kennansalisbury <kennansalisbury@gmail.com>
2020-02-13 15:34:50 -0800

    home page built with placeholders

--- src/Content/Pages/Event.js
+++ src/Content/Pages/Event.js
@@ -1,23 +1,41 @@
 import React from 'react'
 import { Col,
     Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
+    CardTitle, CardSubtitle, Button, CardImgOverlay
   } from 'reactstrap';
 
 const Event = props => {
-    return (
-        <Col>
-            <Card>
-                <CardImg top width="100%" src="https://placeimg.com/200/200/any" alt="Card image cap" />
-                <CardBody>
-                <CardTitle>{props.event.title}</CardTitle>
-                {/* <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText> */}
-                <Button>View More Info</Button>
-                </CardBody>
-            </Card>
-        </Col>
-    )
+    
+    if(props.event) {
+        return (
+            <Col>
+                <Card>
+                    <CardImg top width="100%" src="https://placeimg.com/200/175/any" alt="Card image cap" />
+                    <CardBody>
+                    <CardTitle>{props.event.title}</CardTitle>
+                    {/* <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText> */}
+                    <Button size="sm">View</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        )
+    } else if(props.category) {
+        return(
+            <Col>
+                 <Card inverse>
+                    <CardImg width="100%" src="https://placeimg.com/300/200/any" alt="Card image cap" />
+                    <CardImgOverlay>
+                    <CardTitle><h2><strong>{props.category.title}</strong></h2></CardTitle>
+                    {/* <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                    <CardText>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </CardText> */}
+                    </CardImgOverlay>
+                </Card>
+            </Col>
+        )
+    }
 }
 
 export default Event
--- src/Data/Events.js
+++ src/Data/Events.js
@@ -51,6 +51,112 @@ const Events = {
             "time": "6pm - 10pm"
         }
         
+            ],
+    "near_you": [
+        {
+            "title": "Near You One",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Near You Two",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Near You Three",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Near You Four",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Near You Five",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        }
+        
+            ],
+    "categories": [
+        {
+            "title": "Category One",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Category Two",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Category Three",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Category Four",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        },
+        {
+            "title": "Category Five",
+            "date" : "MONDAY, NOV 25",
+            "loc": "Seattle, WA",
+            "attendies": 10,
+            "id": "i74kjh832",
+            "desc": "Instead of going to a lame Destiny discussion come immerse yourself in the fantastical world building of Sarah King. New players and veterans are welcome!",
+            "host": "Sarah King",
+            "time": "6pm - 10pm"
+        }
+        
             ]
 }
 export default Events;
