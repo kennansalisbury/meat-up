9f627a83d082535dc0100318716092ee6a49ef2c
kennansalisbury <kennansalisbury@gmail.com>
2020-02-13 12:47:30 -0800

    routes created

---
+++ src/Content/Content.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Browse from './Pages/Browse'
+import Home from './Pages/Home'
+import Confirmation from './Pages/Confirmation'
+import Details from './Pages/Details'
+import Signup from './Pages/Signup'
+
+const Content = props => {
+    return (
+        <div>
+            <Route exact path='/' render={
+                () => <Home/>
+            }/>
+            <Route path='/browse' render={
+                () => <Browse/>
+            }/>
+            <Route path='/confirmation' render={
+                () => <Confirmation/>
+            }/>
+            <Route path='/details' render={
+                () => <Details/>
+            }/>
+            <Route path='/signup' render={
+                () => <Signup/>
+            }/>
+
+
+        </div>
+    )
+}
+
+export default Content
