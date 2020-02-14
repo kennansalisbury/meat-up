259743f9e8d0f72889b08f2efc339467ac02b587
kennansalisbury <kennansalisbury@gmail.com>
2020-02-13 20:33:39 -0800

    updated confirmation, testing magic wand cursor

--- src/Content/Pages/Confirmation.js
+++ src/Content/Pages/Confirmation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, Row, Col, Container} from 'reactstrap'
+import {Button, Row, Col, Container, Card, CardImg, CardTitle, CardBody} from 'reactstrap'
 import raven from '../../static/img/Event3.png'
 import {Link} from 'react-router-dom'
 
@@ -30,6 +30,37 @@ const Confirmation = props => {
             <Row>
                 <Button className="sbs-btn">Directions</Button>
                 <Button className="sbs-btn"><Link className="link" to="/browse">Browse More Events</Link></Button>
+            </Row>
+            <Row className="mt">
+                <Row>
+                <h2>Recommended for ye:</h2>
+                </Row>
+
+                <Row>
+                <Col sm="3">
+                <Card>
+                    <CardImg top width="100%" src="https://previews.123rf.com/images/movingmoment/movingmoment1612/movingmoment161200027/70927074-single-grilled-hamburger-patty-isolated-on-white-.jpg" alt="Card image cap" />
+                    <CardBody>
+                    <CardTitle><a className="link-black" href="#">Meat Singles Near You</a></CardTitle>
+                    {/* <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText> */}
+                    {/* <Button className="link" size="sm">View</Button> */}
+                    </CardBody>
+                </Card>
+                </Col>
+                <Col sm="3">
+                <Card>
+                    <CardImg className="card-img" top src="http://www.castlesandmanorhouses.com/pics/suckling.jpg" alt="Card image cap" />
+                    <CardBody>
+                    <CardTitle><a className="link-black" href="#">5 Tips for Meat Prep</a></CardTitle>
+                    {/* <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText> */}
+                    {/* <Button className="link" size="sm">View</Button> */}
+                    </CardBody>
+                </Card>
+                </Col>
+                </Row>
+                
             </Row>
         </Container>   
     )
